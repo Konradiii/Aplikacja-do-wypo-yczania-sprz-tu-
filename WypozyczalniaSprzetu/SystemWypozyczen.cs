b7ac6e6dbdd3e3a1e50e40fd81111b8829181858
Konrad Karczewski <konrad228833@gmail.com>
2026-03-22 16:02:39 +0100

    Dodanie metod odpowiedzialnych za wypozyuczenie oraz oddanie sprzetu

--- WypozyczalniaSprzetu/SystemWypozyczen.cs
+++ WypozyczalniaSprzetu/SystemWypozyczen.cs
@@ -33,4 +33,78 @@ public class SystemWypozyczen
     }
     
     
+     public void WypozyczSprzet(int idSprzetu, Uzytkownik uzytkownik, DateTime dataOddania)
+    {
+        //znajdywanie sprzętu
+        var sprzet = listaSprzetu.FirstOrDefault(x => x.Id == idSprzetu);
+        
+
+        if (sprzet == null)
+        {
+            Console.WriteLine("Sprzet nie znaleziono");
+            return;
+        }
+        //Sprawdza dostępność
+        if (!sprzet.CzyDostepny(DateTime.Now, dataOddania))
+        {
+                Console.WriteLine("Sprzet jest Niedostępny!");
+                return;
+        }
+        //Utworzenie wypozyczenia
+
+        int aktywneWypozyczenia = listaWypozyczen.Count(w =>
+            w.Wypozyczajacy.Id == uzytkownik.Id &&
+            w.FaktycznaDataOddania == null);
+        
+        if (uzytkownik.TypUzytkownika == TypUzytkownika.student && aktywneWypozyczenia >= 2)
+        {
+            Console.WriteLine("Student może mieć maksymalnie 2 aktywne wypożyczenia!");
+            return;
+        }
+        
+        if (uzytkownik.TypUzytkownika == TypUzytkownika.pracownik && aktywneWypozyczenia >= 5)
+        {
+            Console.WriteLine("Pracownik może mieć maksymalnie 5 aktywnych wypożyczeń!");
+            return;
+        }
+
+        
+
+
+        var wypozyczenie = new Wypozyczenie(
+            uzytkownik,
+            sprzet,
+            DateTime.Now,
+            dataOddania
+            );
+        //Dodanie do listy wypozyczen 
+        listaWypozyczen.Add(wypozyczenie);
+        sprzet.Wypozyczenia.Add(wypozyczenie);
+        
+        Console.WriteLine("Wypożyczenie zakończone sukcesem!");
+    }
+
+    public void ZwrocSprzet(int idSprzetu)
+    {
+
+        var wypozyczenie =
+            listaWypozyczen.FirstOrDefault(w => w.WypozyczonySprzet.Id == idSprzetu && w.FaktycznaDataOddania == null);
+
+        if (wypozyczenie == null)
+        {
+            Console.WriteLine("Nie znaleziono zadnego aktywnego wypozyczenia");
+            return;
+        }
+
+        wypozyczenie.FaktycznaDataOddania = DateTime.Now;
+        int dniSpoznienia = wypozyczenie.ObliczAktualneSpoznienie();
+        double oplata =  dniSpoznienia * 100;
+        
+        wypozyczenie.DodatkowaOplata = oplata;
+        Console.WriteLine($"Spóźnienie: {dniSpoznienia} dni");
+        Console.WriteLine($"Opłata dodatkowa: {oplata} zł");
+        Console.WriteLine("Sprzęt zwrócony do wypożyczalni!");
+    }
+    
+    
 }
