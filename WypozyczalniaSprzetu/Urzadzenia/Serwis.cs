b4113d7ee1b9cc4afc4e7189a9afda48f0bbc886
Konrad Karczewski <konrad228833@gmail.com>
2026-03-22 15:58:04 +0100

    Dodanie klasy serwis oraz konstruktora

---
+++ WypozyczalniaSprzetu/Urzadzenia/Serwis.cs
@@ -0,0 +1,17 @@
+﻿namespace WypozyczalniaSprzetu;
+
+public class Serwis
+{
+    public Sprzet Sprzet { get; set; }
+    public DateTime DataSerwisuOd { get; set; }
+    public DateTime DataSerwisuDo { get; set; }
+    public string Komentarz { get; set; }
+
+    public Serwis(Sprzet sprzet, string komentarz)
+    {
+        Sprzet=sprzet;
+        Komentarz=komentarz;
+        DataSerwisuOd=DateTime.Now;
+    }
+    
+}
