f52f113093ea436179abd8116ea250d0c2c61264
Konrad Karczewski <konrad228833@gmail.com>
2026-03-22 16:00:29 +0100

    Dodanie klasy SystemWypozyczen oraz dodanie w niej klasy DodajSprzet

---
+++ WypozyczalniaSprzetu/SystemWypozyczen.cs
@@ -0,0 +1,15 @@
+﻿namespace WypozyczalniaSprzetu;
+
+public class SystemWypozyczen
+{
+    private List<Sprzet> listaSprzetu = new List<Sprzet>();
+    private List<Wypozyczenie> listaWypozyczen = new List<Wypozyczenie>();
+    
+    
+    public void DodajSprzet(Sprzet sprzet)
+    {
+        listaSprzetu.Add(sprzet);
+    }
+    
+    
+}
